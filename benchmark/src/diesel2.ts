a3a387c9dbb604b41bad1477db2da9d917441b7d
pradeepbgs <kumarpradeepbgs@gmail.com>
2025-09-28 16:21:43 +0530

    updated readme & node adaptor works

--- benchmark/src/diesel2.ts
+++ benchmark/src/diesel2.ts
@@ -1,19 +1,21 @@
-import { Diesel } from 'diesel-core'
+import { Diesel, type ContextType } from 'diesel-core'
 //import { HTTPException } from 'diesel-core/http-exception'
 
 
 
-const app = new Diesel()
+const app = new Diesel({
+    
+})
 
 //app.BunRoute('get', '/', 'Hello bun/')
-app.get('/', (ctx) => ctx.text('hello diesel'))
+app.get('/', (ctx:ContextType) => ctx.text('hello diesel'))
 
-app.get('/path/:name', (ctx) => {
-    return ctx.text(`hello ${ctx.params.name} with query: ${ctx.query.name}`)
-})
+// app.get('/path/:name', (ctx) => {
+//     return ctx.text(`hello ${ctx.params.name} with query: ${ctx.query.name}`)
+// })
 
-app.get('/err', () => {
-    throw new HTTPException(400,{message:"err showing"})
-})
+// app.get('/err', () => {
+//     throw new HTTPException(400,{message:"err showing"})
+// })
 
 app.listen(3000)
